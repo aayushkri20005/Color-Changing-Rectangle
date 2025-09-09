b2b7bb9381cc934e1d55a75240c36c3532292e86
Aayush Singh <chhatrapatiaayush2003@gmail.com>
2025-09-09 22:20:04 +0530

    done

--- javasc.js
+++ javasc.js
@@ -4,9 +4,24 @@ rect.addEventListener("mousemove",function(d){
     var rectlocation =rect.getBoundingClientRect();
     var insiderec= d.clientX-rectlocation.left;
     if(insiderec<rectlocation.width/2){
-
+        var redcolour=gsap.utils.mapRange(0,
+            rectlocation.width/2,
+            255,
+            0,
+            insiderec);
+        gsap.to(rect,{
+            backgroundColor:`rgb(${redcolour},0,0)`
+        })
     }
     else{
-        
+        var bluecolour=gsap.utils.mapRange(rectlocation.width/2,
+            rectlocation.width,
+            0,
+            255,
+            insiderec);
+        gsap.to(rect,{
+            backgroundColor:`rgb(0,0,${bluecolour})`
+        })
+
     }
 });
